63ee10f61031110d49cb7b7f21548d7eefbb30ae
kechin.vv <kechinv1@rambler.ru>
2021-12-29 02:11:32 +0400

    Coroutine with lifecycleScope

--- continuewatch63/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ continuewatch63/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -6,13 +6,16 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.*
 
 @DelicateCoroutinesApi
 class MainActivity : AppCompatActivity() {
     private lateinit var prefs: SharedPreferences
     var secondsElapsed: Int = 0
-    lateinit var textSecondsElapsed: TextView
+    private lateinit var textSecondsElapsed: TextView
 
 
     private lateinit var backgroundThread: Job
@@ -23,34 +26,34 @@ class MainActivity : AppCompatActivity() {
         textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
         prefs = getPreferences(Context.MODE_PRIVATE)
         Log.i("test", "onCreate")
+
+        backgroundThread = lifecycleScope.launch(Dispatchers.Default) {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                var prev = System.nanoTime() / 1000000
+                while (this.isActive) {
+                    val cur: Long = System.nanoTime() / 1000000
+                    if (cur != prev + 1000) continue
+                    withContext(Dispatchers.Main) {
+                        textSecondsElapsed.text =
+                            String.format(getString(R.string.seconds), secondsElapsed++)
+                    }
+                    val finish: Long = System.nanoTime() / 1000000
+                    Log.i("time", "Time passed: " + (finish - prev))
+                    prev = cur
+                }
+            }
+        }
     }
 
 
     override fun onPause() {
         super.onPause()
         Log.i("test", "onPause")
-        backgroundThread.cancel()
         prefs.edit().putInt(getString(R.string.time), secondsElapsed).apply()
     }
 
     override fun onResume() {
         secondsElapsed = prefs.getInt(getString(R.string.time), 0)
-        backgroundThread = GlobalScope.launch {
-            val del: Long = System.nanoTime() / 1000000 % 1000
-            var prev = 0L
-            while (this.isActive) {
-                val cur: Long = System.nanoTime() / 1000000
-                if ((cur - del) % 1000 == 0L && cur != prev ) {
-                    textSecondsElapsed.post {
-                        textSecondsElapsed.text =
-                            String.format(getString(R.string.seconds), secondsElapsed++);
-                    }
-                    val finish: Long = System.nanoTime() / 1000000
-                    Log.i("time","Time passed: " + (finish - prev))
-                    prev = cur
-                }
-            }
-        }
         super.onResume()
         Log.i("test", "onResume")
         Log.i("test", "Count of threads: " + Thread.getAllStackTraces().size)
